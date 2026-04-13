f49909b176a5bb7a7023be84829bb5a200ac6853
Noemie <majerusnoemie@gmail.com>
2026-04-13 14:38:15 +0200

    modif db

--- app/db.py
+++ app/db.py
@@ -6,16 +6,8 @@ from embeddings import get_embedding
 
 load_dotenv()
 
-
 def get_db_connection():
-    conn = psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-    )
-    return conn
+    return psycopg2.connect(os.getenv("DATABASE_URL"))
 
 
 
